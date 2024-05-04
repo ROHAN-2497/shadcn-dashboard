a0f7b41499112e33124cc1d4b5ac3d39e7a1cef3
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-05 01:30:06 +0600

    2nd compo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
+import Card, { CardContent, CardProps } from "@/components/Card";
 import PageTitle from "@/components/PageTitle";
+import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
+const cardData: CardProps[] = [
+  {
+    label: "Total Revenue",
+    amount: "$45,231.89",
+    description: "+20.1% from last month",
+    icon: DollarSign,
+  },
+  {
+    label: "Subscriptions",
+    amount: "+2350",
+    description: "+180.1% from last month",
+    icon: Users,
+  },
+  {
+    label: "Sales",
+    amount: "+12,234",
+    description: "+19% from last month",
+    icon: CreditCard,
+  },
+  {
+    label: "Active Now",
+    amount: "+573",
+    description: "+201 since last hour",
+    icon: Activity,
+  },
+];
 
 const Home = () => {
   return (
     <div className="flex flex-col gap-5 w-full">
       <PageTitle title="DashBoard" />
+      <section className="grid grid-cols-1 gap-4 gap-x-8 transition-all sm:grid-cols-2 xl:grid-cols-4">
+        {cardData.map((data, i) => (
+          <Card
+            key={i}
+            amount={data.amount}
+            description={data.description}
+            label={data.label}
+            icon={data.icon}
+          />
+        ))}
+      </section>
+      <section className="grid grid-cols-1 gap-4 transition-all lg:grid-cols-2">
+        <CardContent>
+          <p className="p-4 font-semibold">Overview</p>
+        </CardContent>
+      </section>
     </div>
   );
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,17 +12,15 @@ export type CardProps = {
 export default function Card(props: CardProps): ReactElement {
   return (
     <CardContent>
-      <section>
+      <section className="flex  justify-between gap-2">
         {/* label */}
-        <p>{props.label}</p>
+        <p className="text-sm">{props.label}</p>
         {/*  icon */}
-        <props.icon />
+        <props.icon className="h-4 w-4 text-gray-400" />
       </section>
-      <section>
-        <h2>{props.amount}</h2>
-      </section>
-      <section>
-        <h2>{props.description}</h2>
+      <section className="flex flex-col gap-1">
+        <h2 className="text-2xl font-semibold">{props.amount}</h2>
+        <p className="text-xs text-gray-500">{props.description}</p>
       </section>
     </CardContent>
   );
